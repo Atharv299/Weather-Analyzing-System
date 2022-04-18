e1deba87b98c5270ba6216ae8fca25fc7add0bf2
Atharv299 <72182365+Atharv299@users.noreply.github.com>
2022-04-18 17:14:10 +0530

    Add files via upload

---
+++ quize/jsquiz.js
@@ -0,0 +1,215 @@
+(function() {
+    const myQuestions = [{
+            question: "Which is green house gas among these?",
+            answers: {
+                a: "CO2",
+                b: "NH3",
+                c: "CH4"
+            },
+            correctAnswer: "a"
+
+        },
+        {
+            question: "Factors essential for good environment ?",
+            answers: {
+                a: " many trees in an area than required sufficent",
+                b: "usage of many vehicles",
+                c: "none of these"
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: " A person who studies weather and weather patterns is a(n)",
+            answers: {
+                a: "Geologist",
+                b: "Archeologist",
+                c: "Meterologist",
+
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "A good forecast is one that offers:",
+            answers: {
+                a: "Information based on the most update data",
+                b: "Information on mesoscale effects",
+                c: "All of the above",
+
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Often the greatest and quickest strides in becoming a good forecaster occur through:",
+            answers: {
+                a: " Research",
+                b: "On the job forecasting experience",
+                c: "Meteorology coursework",
+
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "A small circle which determines the location of a weather station is called",
+            answers: {
+                a: "Station model",
+                b: "Stationary model",
+                c: "Station",
+
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "Speed and direction may be measured with help of a",
+            answers: {
+                a: "Anemometer",
+                b: "Weather Ballon",
+                c: "Barometer",
+
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "Which tree consumes more CO2 from environment among these?",
+            answers: {
+                a: "Banyaan",
+                b: "Neem",
+                c: "Teak",
+
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "AQI full form-",
+            answers: {
+                a: "Aeronautics Quantum Infotech",
+                b: "Air Quality Index",
+                c: "Air Quantam Initialization ",
+
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "Why farmers face agricultural problems caused by weather ?",
+            answers: {
+                a: "they don't get weather reasources which gives them real time weather updates ",
+                b: "not having any  technical gagdet to provide them info",
+                c: "Both a and b",
+
+            },
+            correctAnswer: "c"
+        },
+    ];
+
+    function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // we'll want to store the list of answer choices
+            const answers = [];
+
+            // and for each available answer...
+            for (letter in currentQuestion.answers) {
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+               <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+             </label>`
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                `<div class="slide">
+             <div class="question"> ${currentQuestion.question} </div>
+             <div class="answers"> ${answers.join("")} </div>
+           </div>`
+            );
+        });
+
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join("");
+    }
+
+    function showResults() {
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll(".answers");
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = "green";
+            } else {
+                // if answer is wrong or blank
+                // color the answers red
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove("active-slide");
+        slides[n].classList.add("active-slide");
+        currentSlide = n;
+
+        if (currentSlide === 0) {
+            previousButton.style.display = "none";
+        } else {
+            previousButton.style.display = "inline-block";
+        }
+
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = "none";
+            submitButton.style.display = "inline-block";
+        } else {
+            nextButton.style.display = "inline-block";
+            submitButton.style.display = "none";
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    const quizContainer = document.getElementById("quiz");
+    const resultsContainer = document.getElementById("results");
+    const submitButton = document.getElementById("submit");
+
+    // display quiz right away
+    buildQuiz();
+
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    showSlide(0);
+
+    // on submit, show results
+    submitButton.addEventListener("click", showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+})();
